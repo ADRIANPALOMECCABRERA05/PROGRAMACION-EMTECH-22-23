18642bf6f242b76bdfd86963ede9d6fee2aecafa
unknown <apalomec229@gmail.com>
2023-01-09 20:46:44 -0600

    JASON23

---
+++ scripts/funciones.js
@@ -0,0 +1,14 @@
+//function saludarconparametros(nombre){
+    //alert("hola" + nombre + "como estas?");
+//}
+
+//saludarconparametros ("militza")
+//saludarconparametros ("diego")
+
+//const saludar = () => {
+// console.log ("hola como estas")
+//}
+
+//const saludar = nombre => console.log("hola")
+
+
